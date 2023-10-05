d73efad9e004fdf2b6358caedef53bc54052c92c
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-05 19:30:26 +0300

    Doviz Ceviri End

--- Doviz-Ceviri/app.js
+++ Doviz-Ceviri/app.js
@@ -14,11 +14,27 @@ fetch(url + "/codes")
 .then(res => res.json())
 .then(data => {
     const items = data.supported_codes
-    console.log(data)
-    let option
+    let options;
     for(let item of items){
-        option += `<option value=${item[0]}>${item[1]}</option>`
+        options += `<option value=${item[0]}>${item[1]}</option>`;
     }
-    list_one.innerHTML = option
-    list_two.innerHTML = option
+    list_one.innerHTML = options
+    list_two.innerHTML = options
+})
+
+calculate.addEventListener('click',function(){
+    const doviz1 = currency_one.value
+    const doviz2 = currency_two.value
+    const miktar = amount.value
+    console.log(doviz1,doviz2,miktar)
+    fetch(url + "/latest/" + doviz1)
+    .then(res => res.json())
+    .then(data => {
+        result.innerHTML = `
+            <div class="card border-primary">
+                <div class="card-body text-center" style="font-size: 30px;">
+                ${miktar} ${doviz1} = ${data.conversion_rates[doviz2]*miktar} ${doviz2}</div>
+            </div>
+        `
+    })
 })
