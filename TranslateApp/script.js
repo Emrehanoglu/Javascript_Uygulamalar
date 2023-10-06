757fd61bfc7889fd70ab33b24a8a203e4c276dc3
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-06 22:58:15 +0300

    Translate APP/API #2

--- TranslateApp/script.js
+++ TranslateApp/script.js
@@ -1,5 +1,8 @@
 const fromLang = document.querySelector('#from-lang')
 const toLang = document.querySelector('#to-lang')
+const btnTranslate = document.querySelector('#btnTranslate')
+const fromText = document.querySelector('#from-text')
+const toText = document.querySelector('#to-text')
 
 for(let lang in languages){
     let option = `<option value="${lang}">${languages[lang]}</option>`
@@ -10,3 +13,18 @@ for(let lang in languages){
     fromLang.value = "tr-TR"
     toLang.value = "en-GB"
 }
+
+btnTranslate.addEventListener('click',() => {
+    let text = fromText.value
+    let from = fromLang.value
+    let to = toLang.value
+    
+    const url = `https://api.mymemory.translated.net/get?q=${text}&langpair=${from}|${to}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        toText.value = data.responseData.translatedText
+    })
+})
+
