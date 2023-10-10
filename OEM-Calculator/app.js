e9c97ab18ebb7755c352021a310b01000d920962
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-10 21:52:02 +0300

    OEM Calculator #3

--- OEM-Calculator/app.js
+++ OEM-Calculator/app.js
@@ -6,6 +6,33 @@ const StorageController = (function(){
 // Product Controller
 const ProductController = (function(){
 
+    //private
+    const Product = function(id,name,price){
+        this.id = id
+        this.name = name
+        this.price = price
+    }
+
+    const data = {
+        products : [
+            {id:0, name:'Monitör',price:100},
+            {id:1, name:'Ram',price:30},
+            {id:2, name:'Klavye',price:10}
+        ],
+        selectedProduct : null,
+        totalPrice : 0
+    }
+
+    return{
+        //public
+        getProducts : function(){
+            return data.products
+        },
+        getData : function(){
+            return data
+        }
+    }
+
 })()
 
 // UI Controller, arayüz üzerindeki html' ler ile çalıştığım kısım
@@ -14,6 +41,16 @@ const UIController = (function(){
 })()
 
 // App Controller, ana modül işlemleri
-const AppController = (function(ProductCtrl, UICtrl){
+const App = (function(ProductCtrl, UICtrl){
+
+    return {
+        init : function(){
+            console.log('starting app...')
+            const products = ProductCtrl.getProducts()
+            console.log(products)
+        }
+    }
 
 })(ProductController,UIController)
+
+App.init()
