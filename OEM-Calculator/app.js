b16c7cfd4dd07049d45be154a07f986c457fd8e3
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-10 21:42:35 +0300

    OEM Calculator #2

---
+++ OEM-Calculator/app.js
@@ -0,0 +1,19 @@
+// Storage Controller, kullanıcıdan aldığımız bilgileri tarayıcı belleğine attığım kısım
+const StorageController = (function(){
+    
+})()
+
+// Product Controller
+const ProductController = (function(){
+
+})()
+
+// UI Controller, arayüz üzerindeki html' ler ile çalıştığım kısım
+const UIController = (function(){
+
+})()
+
+// App Controller, ana modül işlemleri
+const AppController = (function(ProductCtrl, UICtrl){
+
+})(ProductController,UIController)
