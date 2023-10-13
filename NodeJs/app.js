26ea0cb06ab9ae2950f3927f7278bf1505f1f91c
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-14 02:10:29 +0300

    NodeJS #3

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -4,3 +4,10 @@ const logger = require('./logger')
 logger.log('nodeJs')
 console.log(logger.name)
 
+//Undescore
+
+const _ = require('underscore')
+
+var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 70}, {name: 'curly', age: 60}];
+console.log(_.max(stooges, function(stooge){ return stooge.age; }));
+
