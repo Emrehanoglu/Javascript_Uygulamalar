b9087e20dc20f025b17cc08f3d2e2c53da960a7b
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-16 21:36:04 +0300

    MovieApp #2

--- MovieApp/src/js/index.js
+++ MovieApp/src/js/index.js
@@ -2,18 +2,10 @@
 //url : https://api.themoviedb.org/3
 //https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1
 
-async function getMovie(keyword){
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDlhZjdlYjBkNzczNjA1OWRmNDQxNmNmZmY4MWRiMCIsInN1YiI6IjY1MmQ2Y2E2MDI0ZWM4MDEwMTUzMWZhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HaFx5bCQEQiQEKTRS8UY8meGQrpb2_gjPuVytMorE-I'
-    }
-  };
-  
-  const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=en-US&page=1`, options)
-  const data = await response.json()
-  console.log(data.results[0].id)
-}
+import Search from "./models/Search";
 
-getMovie('abc')
+const search = new Search('abc')
+
+console.log(search)
+
+search.getMovie()
---
+++ MovieApp/src/js/models/Search.js
@@ -0,0 +1,20 @@
+//Search sınıfından bir nesne türetildiği zaman içerisinde keyword gönderilecek
+export default class Search{
+    constructor(keyword){
+        this.keyword = keyword
+    }
+
+    async getMovie(){
+        const options = {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDlhZjdlYjBkNzczNjA1OWRmNDQxNmNmZmY4MWRiMCIsInN1YiI6IjY1MmQ2Y2E2MDI0ZWM4MDEwMTUzMWZhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HaFx5bCQEQiQEKTRS8UY8meGQrpb2_gjPuVytMorE-I'
+          }
+        };
+        
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${this.keyword}&include_adult=false&language=en-US&page=1`, options)
+        this.data = await response.json()
+        console.log(this.data.results)
+      }      
+}
